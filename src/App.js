22a5f9d542961bb194afff9c0c2068c453cf26a3
DESKTOP-4SPN8C3\deit <kjr04205>
2023-02-14 15:41:48 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("처음 실행했을떄만");
+  }, []);
+  useEffect(() => {
+    console.log("키워드 바꼇을때 실행");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("카운터가 바꼈을때 실행");
+  }, [counter]);
+  useEffect(() => {
+    console.log("카운터 또는 키워드가 바꼈을때 실행");
+  }, [counter, keyword]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input 
+        value={keyword} 
+        onChange={onChange} 
+        type="text"
+        placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
